9989609262e32a803d0a08cc09c1c22199a77e77
esraunal <47426593+unalesra@users.noreply.github.com>
2021-02-02 20:34:20 +0300

    UnitOfWork eklendi

---
+++ ProgrammersBlog.Data/Abstract/IUnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Data.Abstract
+{
+    public interface IUnitOfWork: IAsyncDisposable
+    {
+        IArticleRepository Articles { get; }
+        ICategoryRepository Categories { get; }
+        ICommentRepository Comments { get; }
+        IRoleRepository Roles { get; }
+        IUserRepository Users { get; }
+
+        //veritabanına işlemleri kaydetmek
+        //burada eklememin sebepler
+        /*
+         * 1-bir sürü ekleme işlemi yapsam da tek save ile işi çözebileceğim
+         * 2-eğer eklemelerin birinde hata oluşursa, bu veriler veritabanına hiç ulaşamamış olacaklar. Böylece veritabanına eksik bilgi gitmemiş olacak.
+         */
+        Task<int> SaveAsync();
+
+    }
+}
---
+++ ProgrammersBlog.Data/Concreate/UnitOfWork.cs
@@ -0,0 +1,51 @@
+﻿using ProgrammersBlog.Data.Abstract;
+using ProgrammersBlog.Data.Concreate.EntityFramework.Contexts;
+using ProgrammersBlog.Data.Concreate.EntityFramework.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Data.Concreate
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ProgrammersBlogContext _context;
+
+        private EfArticleRepository _articleRepository;
+        private EfCategoryRepository _categoryRepository;
+        private EfCommentRepository _commentRepository;
+        private EfRoleRepository _roleRepository;
+        private EfUserRepository _userRepository;
+
+        public UnitOfWork(ProgrammersBlogContext context)
+        {
+            _context = context;
+        }
+
+        public async ValueTask IAsyncDisposable()
+        {
+            await _context.DisposeAsync();
+        }
+
+        //?? null coalescing
+        //eğer değer null gelmişse şu işlemi yap 
+        //int x ?? x=10 gibi
+        public IArticleRepository Articles => _articleRepository ?? new EfArticleRepository(_context); //_article repository'i döndür diyorum. somut halini verdim. Eğer nesne oluşturulmamışsa oluştur demiş oldum ?? ile
+
+        public ICategoryRepository Categories => _categoryRepository ?? new EfCategoryRepository(_context);
+
+        public ICommentRepository Comments =>_commentRepository ?? new EfCommentRepository(_context);
+
+        public IRoleRepository Roles => _roleRepository ?? new EfRoleRepository(_context);
+
+        public IUserRepository Users => _userRepository ?? new EfUserRepository(_context);
+
+        public async Task<int> SaveAsync()
+        {
+            //burada int dememe gerek yok savechanges dediğimde zaten int değeriyle beraber döndürüyor
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
