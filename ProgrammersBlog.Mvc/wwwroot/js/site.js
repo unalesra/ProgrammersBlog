334ff5534ab8ccd8b332f43f64afd218b1b0a56a
esraunal <47426593+unalesra@users.noreply.github.com>
2021-03-04 21:05:15 +0300

    convertFirstLetterToUpperCase, convertToShortDate yazıldı. Eklenememe durumunda toastr eklendi.

---
+++ ProgrammersBlog.Mvc/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿function convertFirstLetterToUpperCase(text) {
+
+    //charAt ile verdiğim indexteki harfi alıyorum.
+    //slice ile verilen indexten itibaren verilen string ifade alınır.
+    return text.charAt(0).toUpperCase() + text.slice(1);
+}
+
+function convertToShortDate(dateString) {
+    const shortDate = new Date(dateString).toLocaleDateString('en-US');
+    return shortDate;
+}
